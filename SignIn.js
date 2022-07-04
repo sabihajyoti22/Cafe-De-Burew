6992366c9e105e2ef3931cc040caedb6ee97b0f8
SabihaJyoti <sabihajyoti@gmail.com>
2022-07-04 23:09:28 +0600

    second commit

--- SignIn.js
+++ SignIn.js
@@ -37,6 +37,6 @@ signInForm.addEventListener("submit",(e)=>{
         passwordInput.value=""
         rePasswordInput.value=""
         localStorage.setItem("Users",JSON.stringify(users));
-        window.location.replace("./index.html")
+        window.location.replace("/")
     }
 })
